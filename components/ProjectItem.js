27e95c7b66c17935db49c80c8ea63274932ad549
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-03 06:06:08 +0300

    Update ProjectItem.js

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const ProjectItem = ({ title, image, projectUrl, languages }) => {
+const ProjectItem = ({ title, image, projectUrl }) => {
   return (
     <div className="relative cursor-pointer hover:scale-105 mx-auto transition-all duration-500 flex items-center justify-center h-70 w-full shadow-xl shadow-gray-400 rounded-xl px-2 py-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#00b4db]/50 ease-in-out duration-500">
       <Image
@@ -16,7 +16,7 @@ const ProjectItem = ({ title, image, projectUrl, languages }) => {
         <h3 className="md:text-2xl text-xl  text-white text-center tracking-wider">
           {title}
         </h3>
-        <p className="py-2 text-white  text-center">{languages}</p>
+     
         <Link href={projectUrl}>
           <p className="text-center py-2 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More Info
