8132e436eeac806bb1363c5ed53303b6142e5ba7
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 22:45:25 +0300

    Update house.js

--- pages/house.js
+++ pages/house.js
@@ -17,20 +17,18 @@ function house() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">Real Estate Site</h2>
-          <p>React JS / Tailwind CSS </p>
         </div>
       </div>
 
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5  gap-8 pt-5">
         <div className="col-span-4">
-          <p className="uppercase text-xltracking-widest text-[#5651e5] ">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5] ">
             Project
           </p>
           <h2 className="py-2 text-gray-600">Overview</h2>
 
           <p>
-            This is a house hunting real estate site. It is built with React JS
-            and Bootstrap CSS. It has a filter system that allows users to
+            This is a house hunting real estate site. It has a filter system that allows users to
             choose the type of house they want to buy. It also has a system that
             allows users to save houses they like . It also has a video for each
             house that is displayed on the site.
@@ -44,33 +42,8 @@ function house() {
               Live Demo
             </a>
           </button>
-          <button className="px-8 py-2 mt-4">
-            <a
-              href="https://github.com/MICHAELMUNAVU83/housing-frontend"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Code
-            </a>
-          </button>
+          
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 md:text-md text-sm py-2 ">
-                <RiRadioButtonFill className="pr-1" /> React JS
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1  md:text-md text-sm" />{" "}
-                Tailwind CSS
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1  md:text-md text-sm" /> Ruby
-                on Rails
-              </p>
-            </div>
-          </div>
         </div>
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
