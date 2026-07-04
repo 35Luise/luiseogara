fb0e1460bbaa0fd7c88cdb7a76a02a79be47ab02
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 23:00:34 +0300

    Update tenx.js

--- pages/tenx.js
+++ pages/tenx.js
@@ -17,13 +17,12 @@ function tenx() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">10x Beast</h2>
-          <p>Elixir / Phoenix Live View / Python</p>
         </div>
       </div>
 
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5  gap-8 pt-5">
         <div className="col-span-4">
-          <p className="uppercase text-xltracking-widest text-[#5651e5] ">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5] ">
             Project
           </p>
           <h2 className="py-2 text-gray-600">Overview</h2>
@@ -54,22 +53,6 @@ function tenx() {
               Video Demo
             </a>
           </button>
-        </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Elixir
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Phoenix Live View
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
         </div>
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
