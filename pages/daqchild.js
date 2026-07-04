a06f209b1e9dbcf8c8340b3e6c368b094c185165
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 20:37:11 +0300

    Update daqchild.js

--- pages/daqchild.js
+++ pages/daqchild.js
@@ -17,7 +17,6 @@ function daqchild() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">Gibbz Tha Daqchild Official Website</h2>
-          <p>Ruby on Rails / React JS / Tailwind CSS </p>
         </div>
       </div>
 
@@ -28,8 +27,7 @@ function daqchild() {
           </p>
           <h2 className="py-2 text-gray-600">Overview</h2>
           <p>
-            This is the official website of the artist Gibbz Tha Daqchild. It is
-            built with Ruby on Rails and React JS. It allows his fans to
+            This is the official website of the artist Gibbz Tha Daqchild. It allows his fans to
             interact and download his mixes and music. They can also buy
             merchandises and get in touch with him to book him for events .
           </p>
@@ -42,31 +40,9 @@ function daqchild() {
              Live Demo
             </a>
           </button>
-          <button className="px-8 py-2 mt-4">
-            <a
-              href="https://github.com/MICHAELMUNAVU83/daqchild-frontend"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Code
-            </a>
-          </button>
+          
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 py-2  md:text-md text-sm ">
-                <RiRadioButtonFill className="pr-1 " /> Ruby on Rails
-              </p>
-              <p className="flex items-center text-gray-600  md:text-md text-sm py-2 ">
-                <RiRadioButtonFill className="pr-1" /> React JS
-              </p>
-              <p className="flex items-center text-gray-600  md:text-md text-sm py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
+        
         </div>
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
