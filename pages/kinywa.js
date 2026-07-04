4dad030dd372d45f7b633c3704738010999b5e28
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 22:32:07 +0300

    Update kinywa.js

--- pages/kinywa.js
+++ pages/kinywa.js
@@ -17,19 +17,17 @@ function kinywa() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">Photographer Portfolio</h2>
-          <p>React JS / Tailwind CSS </p>
         </div>
       </div>
 
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5  gap-8 pt-5">
         <div className="col-span-4">
-          <p className="uppercase text-xltracking-widest text-[#5651e5] ">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5] ">
             Project
           </p>
           <h2 className="py-2 text-gray-600">Overview</h2>
           <p>
-            This is a photographer portfolio website built with React JS and
-            Tailwind CSS . It has a slider that displays the photographer's work
+            This is a photographer portfolio website that has a slider that displays the photographer's work
             and a contact form that allows users to send messages to the
             photographer.
           </p>
@@ -42,28 +40,6 @@ function kinywa() {
               Live Demo
             </a>
           </button>
-          <button className="px-8 py-2 mt-4">
-            <a
-              href="https://github.com/MICHAELMUNAVU83/kinywa"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Code
-            </a>
-          </button>
-        </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> React JS
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
         </div>
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
