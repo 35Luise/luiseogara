bb21d7bfc38d80998e0419d07a0e943fa65ef5c4
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 20:40:56 +0300

    Update engagepro.js

--- pages/engagepro.js
+++ pages/engagepro.js
@@ -17,7 +17,7 @@ function engagepro() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">EngagePro Employee Management System</h2>
-          <p>Elixir / Phoenix Live View / Tailwind CSS</p>
+          
         </div>
       </div>
 
@@ -53,34 +53,9 @@ function engagepro() {
                 Video Demo
               </a>
             </button>
-            <button className="md:px-8 px-2 py-2 ml-4 mt-4">
-              <a
-                href="https://github.com/MICHAELMUNAVU83/employee_management_system"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Code
-              </a>
-            </button>
           </div>
         </div>
 
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Elixir
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Phoenix Live View
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
-        </div>
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
         </Link>
