fd98fca268485ef170bf56d6cdaaf3353e36613c
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 21:12:01 +0300

    Update peopleschoice.js

--- pages/peopleschoice.js
+++ pages/peopleschoice.js
@@ -17,7 +17,6 @@ function peopleschoice() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">People's Choice Africa Awards </h2>
-          <p> React JS / Tailwind CSS </p>
         </div>
       </div>
 
@@ -43,29 +42,8 @@ function peopleschoice() {
               Live Demo
             </a>
           </button>
-          <button className="px-8 py-2 mt-4">
-            <a
-              href="https://github.com/Kimutai01/people-s-choice"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Code
-            </a>
-          </button>
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600  md:text-md text-sm py-2 ">
-                <RiRadioButtonFill className="pr-1" /> React JS
-              </p>
-              <p className="flex items-center text-gray-600  md:text-md text-sm py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
-        </div>
+        
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
         </Link>
