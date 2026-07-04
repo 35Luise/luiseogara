711b46a2c13c69bf4db8746e9f1bc0dadc94025e
35Luise <162688303+35Luise@users.noreply.github.com>
2026-07-04 21:55:47 +0300

    Update thinkopal.js

--- pages/thinkopal.js
+++ pages/thinkopal.js
@@ -17,19 +17,17 @@ function thinkopal() {
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] z-10 text-white p-2">
           <h2 className="py-2">Thinkopal Website</h2>
-          <p>React JS / Tailwind CSS </p>
         </div>
       </div>
 
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5  gap-8 pt-5">
         <div className="col-span-4">
-          <p className="uppercase text-xltracking-widest text-[#5651e5] ">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5] ">
             Project
           </p>
           <h2 className="py-2 text-gray-600">Overview</h2>
           <p>
-            This is a website for a company called Thinkopal. It is built with
-            React JS and Tailwind CSS for styling. Thinkopal helps small
+            This is a website for a company called Thinkopal. Thinkopal helps small
             businesses to grow by providing them with the right tools .
           </p>
           <button className="px-8 py-2  mt-4 mr-8">
@@ -41,29 +39,9 @@ function thinkopal() {
               Demo
             </a>
           </button>
-          <button className="px-8 py-2 mt-4">
-            <a
-              href="https://github.com/MICHAELMUNAVU83/thinkopal"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Code
-            </a>
-          </button>
+          
         </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center  font-semibold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1 ">
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> React JS
-              </p>
-              <p className="flex items-center text-gray-600 py-2 ">
-                <RiRadioButtonFill className="pr-1" /> Tailwind CSS
-              </p>
-            </div>
-          </div>
-        </div>
+        
         <Link href="/#projects">
           <p className="underline cursor-pointer">Back </p>
         </Link>
